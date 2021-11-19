156d969fd2047b072466173f35b68729d5eb9519
kuns019 <56548175+kuns019@users.noreply.github.com>
2021-11-19 22:31:42 +0700

    Update singin.js

--- googleLogin/singin.js
+++ googleLogin/singin.js
@@ -2,13 +2,10 @@ import * as Google from "expo-google-app-auth";
 import React, { useState } from "react";
 import firebase from "../database/firebaseDB";
 
-
-
 export async function signInWithGoogleAsync() {
     
     try {
         const userCollection = firebase.firestore().collection('users');
-        const token = firebase.firestore().collection('token').doc("ilDoD6eYPTy63VhbyCL1")
         const result = await Google.logInAsync({
             androidClientId:
                 "1011125952941-sn36odhinio6knb15n3q8v78k9e2gk43.apps.googleusercontent.com",
@@ -16,51 +13,58 @@ export async function signInWithGoogleAsync() {
         });
 
         // console.log(result);
-        const fname = result.user.givenName;
-        const lname = result.user.familyName;
-        const email = result.user.email;
-        const image = result.user.photoUrl;
+        const rest_fname = result.user.givenName;
+        const rest_lname = result.user.familyName;
+        const rest_email = result.user.email;
+        const rest_image = result.user.photoUrl;
 
         if (result.type === "success") {
-            let accessToken = '';
-            
-            await token.get().then(res => {
-                const { user } = res.data();
-                accessToken = user
-            });
-            
-            console.log(accessToken);
-            if (!accessToken) {
-                console.log("Not Have Token");
-                userCollection.onSnapshot(async(query) => {
-                    await query.forEach(res => {
-                        // This is the problem that make me crazy and dunno shit to do this project in this semister and i will let it go to da moon
-                        if (res.data().email == email){
-                            token.set({
-                                user: res.id
-                            })
-                            console.log("Already have account");
-                        } else {
-                            console.log("New Account please add one");
-                        }
+            let list_users = [];
+            let found_account = false;
 
-                    });
-
-                })
-                console.log("add account "+haveAccount);
-                if (!accessToken){
-                    userCollection.add({
-                        firstname: fname,
-                        lastname: lname,
+            await userCollection.get().then(async items => {
+                await items.forEach(res => {
+                    const { email, fname, lname, picture, isLogin } = res.data();
+                    console.log("Email: "+email);
+                    
+                    if(email === rest_email){
+                        userCollection.doc(res.id).update({
+                            isLogin: true
+                        })
+                        found_account = true;
+                        console.log("Update Success");
+                    } 
+                    
+                    list_users.push({
+                        user_id: res.id,
                         email: email,
-                        picture: image,
-                        role: "user"
+                        fname: fname,
+                        lname: lname,
+                        image: picture,
+                        isLogin: isLogin
                     });
-                    console.log("add success");
-                } 
-            
-            }
-            
+                }); 
+                
+                if (!found_account) {
+                    userCollection.add({
+                        email: rest_email,
+                        firstname: rest_fname,
+                        lastname: rest_lname,
+                        picture: rest_image,
+                        role: "user",
+                        isLogin: true
+                    })
+                    console.log("Add Success");
+                }
+
+                list_users.forEach(items => {
+                    if (items.email.includes(rest_email)) {
+                        // sent item out with props
+                        console.log(rest_email + " have in list");
+                    }
+                })
+            })
+
             return result;
         } else {
             return { cancelled: true };
@@ -69,7 +73,6 @@ export async function signInWithGoogleAsync() {
         return { error: true };
     }
 }
-
 
 
 // Create a reference to the cities collection import { collection, query, where } from "firebase/firestore"; const citiesRef = collection(db, "cities"); 
